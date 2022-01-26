754de3593e575b2180ad0bbc43abaab17848cd8a
TianZhonglin001 <82213646+TianZhonglin001@users.noreply.github.com>
2022-01-26 13:14:48 +0800

    submit first assignment

---
+++ 第一次作业/SZ202104063田忠霖/第一次作业第一题.cpp
@@ -0,0 +1,53 @@
+﻿// 2022_1_26work.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+class Person
+{
+public:
+	string name;
+	string sex;
+	int age;
+	Person(void);
+	void PersonSet(string name, string sex, int age);
+	Person( const Person &obj);
+	void PersonPrint(void);
+	~Person(void);
+	Person* ptr;	
+};
+Person::Person(void) {
+	name = "初始化姓名";
+	sex = "初始化性别";
+	age = 0;
+}
+void Person::PersonSet(string name_input, string sex_input, int age_input) {
+	name = name_input;
+	sex = sex_input;
+	age = age_input;
+	cout << "Structure Success!" << endl;
+}
+Person::Person(const Person &obj) {
+	name = obj.name;
+	sex = obj.sex;
+	age = obj.age;
+	cout << "Copy Success!"  << endl;
+}
+void Person::PersonPrint() {
+	cout << "姓名:" << name << " 性别:" << sex << " 年龄:" << age << endl;
+}
+Person::~Person() {
+}
+int main()
+{
+	Person p1;
+	Person p2;
+
+	p1.PersonSet("一号玩家", "男", 22);
+	p1.PersonPrint();
+
+	p2.PersonSet("二号玩家", "女", 20);
+
+	Person p3(p2);
+	p3.PersonPrint();
+}
---
+++ 第一次作业/SZ202104063田忠霖/第一次作业第二题.cpp
@@ -0,0 +1,70 @@
+﻿// 2022_1_26work2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+int student_sum = 0;
+int grade_sum = 0;
+class Stud
+{
+public:
+	string id;
+	string name;
+	int grade;
+	void StudSet(string id_input, string name_input , int grade_input);
+	void StuPrint();
+	void SumPrint();
+};
+
+void Stud::SumPrint() {
+	cout << "总人数:" << student_sum << " 总分:" << grade_sum << " 平均成绩:" << double(grade_sum)/ double(student_sum) << endl;
+}
+void Stud::StuPrint() {
+	cout << "学号:" << id << " 姓名:" << name << " 成绩:" << grade <<endl;
+}
+void Stud::StudSet(string id_input, string name_input, int grade_input)
+{
+	id = id_input;
+	name = name_input;
+	grade = grade_input;
+	student_sum += 1;
+	grade_sum += grade_input;
+
+	cout << "Structure success!" << endl;
+}
+int main()
+{
+	int i;
+	cout << "输入记录个数: " << endl;
+	cin >> i;
+    Stud stud_list[10] = {};
+	int index = 0;
+	while (index<i)
+	{
+		Stud stud;
+		string id_input;
+		string name_input;
+		int grade;
+		cout << "输入学号: " << endl;
+		cin >> id_input;
+		cout << "输入姓名: " << endl;
+		cin >> name_input;
+		cout << "输入成绩: " << endl;
+		cin >> grade;
+		stud.StudSet(id_input, name_input, grade);
+		stud_list[index] = stud;
+		cout << "是否继续(y/n): " << endl;
+		index++;
+		char order;
+		cin >> order;
+		if (order != 'y') {
+			break;
+		}
+	}
+	for (i = 0; i < index; i++) {
+		stud_list[i].StuPrint();
+	}
+	stud_list[index].SumPrint();
+	return 0;
+}
+
