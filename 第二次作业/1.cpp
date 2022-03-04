340786f79fb44e662c7e3da938eab4e12c35f416
edgeworker-auto <95603484+edgeworker-auto@users.noreply.github.com>
2022-03-04 14:29:07 +0800

    Add files via upload

---
+++ 第二次作业/1.cpp
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include<iostream> 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include"quick.h"
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    QuickDemo dx;
+    dx.video_demo();
+
+    return 0;
+}
